ec34cce8c84776fde9eaef931076f4f1e20d9552
chenkaijie <chenkj@hadlinks.com>
2016-10-23 19:09:13 +0800

    第一次增加SecondTest文件

---
+++ SecondTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SecondTest
+//
+//  Created by chenkaijie on 16/10/23.
+//  Copyright © 2016年 chenkaijie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SecondTest/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  SecondTest
+//
+//  Created by chenkaijie on 16/10/23.
+//  Copyright © 2016年 chenkaijie. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+    1
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
