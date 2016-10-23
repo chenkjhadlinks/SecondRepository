21dac59f4a3f313a7ab8f3e26653e3073f681a22
chenkaijie <chenkj@hadlinks.com>
2016-10-23 20:07:11 +0800

    AppDelegate   kj 增加一行代码 1

--- SecondTest/AppDelegate.m
+++ SecondTest/AppDelegate.m
@@ -17,6 +17,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    NSLog(@"kj 增加一行代码 1 ");
+    
     return YES;
 }
 
